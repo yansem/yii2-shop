ef45543aaf646e59e0643563f0ec07c462ba6ffc
SEMA <janchevsem@mail.ru>
2021-10-04 13:05:50 +0500

    menu init

---
+++ components/MenuWidget.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\components;
+
+use yii\base\Widget;
+
+class MenuWidget extends Widget
+{
+    public $tpl;
+    public $ul_class;
+    public $data;
+    public $menuHtml;
+
+    public function init()
+    {
+        parent::init();
+        if($this->ul_class === null){
+            $this->ul_class = 'menu';
+        }
+        if($this->tpl === null){
+            $this->tpl = 'menu';
+        }
+        $this->tpl .= '.php';
+    }
+
+    public function run()
+    {
+        return $this->tpl;
+    }
+}
