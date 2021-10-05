75dfb69e254940c1a79013bc1df85b39abbe15e0
SEMA <janchevsem@mail.ru>
2021-10-05 16:17:01 +0500

    cart: ajax

---
+++ controllers/CartController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\controllers;
+
+class CartController extends AppController
+{
+    public function actionAdd($id)
+    {
+        var_dump($id); die;
+    }
+}
--- web/js/main.js
+++ web/js/main.js
@@ -62,3 +62,19 @@ paypal.minicart.cart.on('checkout', function (evt) {
         evt.preventDefault();
     }
 });
+
+$('.add-to-cart').on('click', function (){
+    let id = $(this).data('id');
+    $.ajax({
+        url: 'cart/add',
+        data: {id: id},
+        type: 'GET',
+        success: function (res){
+            console.log(res);
+        },
+        error: function (){
+            alert('Error!')
+        }
+    })
+    return false;
+})
