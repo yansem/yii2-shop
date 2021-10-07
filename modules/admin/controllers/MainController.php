a4842014069af5b263d6036d64bd5da3fb23a1d6
SEMA <janchevsem@mail.ru>
2021-10-07 15:55:02 +0500

    admin: stats page

--- modules/admin/controllers/MainController.php
+++ modules/admin/controllers/MainController.php
@@ -3,11 +3,19 @@
 namespace app\modules\admin\controllers;
 
 
+use app\modules\admin\models\Category;
+use app\modules\admin\models\Order;
+use app\modules\admin\models\Product;
+
 class MainController extends AppAdminController
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $orders = Order::find()->count();
+        $products = Product::find()->count();
+        $categories = Category::find()->count();
+
+        return $this->render('index', compact('orders', 'products', 'categories'));
     }
 
 }
--- modules/admin/views/main/index.php
+++ modules/admin/views/main/index.php
@@ -4,3 +4,56 @@ $this->title = 'Статистика магазина';
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
+
+<div class="row">
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-aqua">
+            <div class="inner">
+                <h3><?= $orders ?></h3>
+
+                <p>Заказы</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-shopping-cart"></i>
+            </div>
+            <a href="<?= \yii\helpers\Url::to(['order/index']) ?>" class="small-box-footer">
+                More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-green">
+            <div class="inner">
+                <h3><?= $products ?><sup style="font-size: 20px">%</sup></h3>
+
+                <p>Продукты</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-cutlery"></i>
+            </div>
+            <a href="<?= \yii\helpers\Url::to(['product/index']) ?>" class="small-box-footer">
+                More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-yellow">
+            <div class="inner">
+                <h3><?= $categories ?></h3>
+
+                <p>Категории</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-cubes"></i>
+            </div>
+            <a href="<?= \yii\helpers\Url::to(['category/index']) ?>" class="small-box-footer">
+                More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div>
+</div>
