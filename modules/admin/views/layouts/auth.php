e0f17a76dc18178e14e6ca84e9ed5ab9ac848554
SEMA <janchevsem@mail.ru>
2021-10-07 10:51:02 +0500

    admin auth layout

--- modules/admin/views/layouts/auth.php
+++ modules/admin/views/layouts/auth.php
@@ -1,1 +1,40 @@
 <?php
+
+use app\assets\AuthAsset;
+use yii\helpers\Html;
+
+AuthAsset::register($this);
+?>
+
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <base href="/adminlte/">
+    <meta charset="<?= Yii::$app->charset ?>">
+    <?php $this->registerCsrfMetaTags() ?>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?= Html::encode($this->title) ?></title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <?php $this->head() ?>
+
+    <!-- Bootstrap 3.3.7 -->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Google Font -->
+</head>
+<body class="hold-transition login-page">
+<?php $this->beginBody() ?>
+
+    <?= $content; ?>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
