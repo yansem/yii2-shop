7bbeb13982eec996577950ceb5bf589dc328bb68
SEMA <janchevsem@mail.ru>
2021-10-07 11:08:59 +0500

    admin auth: access filter

--- modules/admin/controllers/AppAdminController.php
+++ modules/admin/controllers/AppAdminController.php
@@ -2,9 +2,28 @@
 
 namespace app\modules\admin\controllers;
 
+use yii\filters\AccessControl;
 use yii\web\Controller;
 
 class AppAdminController extends Controller
 {
-
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['login'],
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
 }
