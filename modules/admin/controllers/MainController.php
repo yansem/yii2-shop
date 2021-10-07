47b07b698500066cdd67cbc083e3d805651d6c43
SEMA <janchevsem@mail.ru>
2021-10-07 11:09:49 +0500

    AppAdminController

--- modules/admin/controllers/MainController.php
+++ modules/admin/controllers/MainController.php
@@ -2,9 +2,8 @@
 
 namespace app\modules\admin\controllers;
 
-use yii\web\Controller;
 
-class MainController extends Controller
+class MainController extends AppAdminController
 {
     public function actionIndex()
     {
