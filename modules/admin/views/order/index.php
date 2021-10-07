7029d475844b3b1811588a287b668708fafadbfa
SEMA <janchevsem@mail.ru>
2021-10-07 19:18:48 +0500

    admin order: status

--- modules/admin/views/order/index.php
+++ modules/admin/views/order/index.php
@@ -28,6 +28,13 @@ $this->params['breadcrumbs'][] = $this->title;
                             'updated_at',
                             'qty',
                             'total',
+                            [
+                                    'attribute' => 'status',
+                                    'value' => function($data){
+                                        return $data->status ? '<span class="text-green">Завершен</span>' : '<span class="text-red">Новый</span>';
+                                    },
+                                    'format' => 'raw',
+                            ],
                             //'status',
                             //'name',
                             //'email:email',
