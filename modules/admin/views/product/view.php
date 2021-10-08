f4d50243def50b1cb5329a7f2bf0047dc4972321
SEMA <janchevsem@mail.ru>
2021-10-08 18:37:11 +0500

    admin prod: view

--- modules/admin/views/product/view.php
+++ modules/admin/views/product/view.php
@@ -6,40 +6,51 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\modules\admin\models\Product */
 
-$this->title = $model->title;
-$this->params['breadcrumbs'][] = ['label' => 'Products', 'url' => ['index']];
+$this->title = "Товар: " . $model->title;
+$this->params['breadcrumbs'][] = ['label' => 'Товары', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="product-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="row">
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-header with-border">
+                <?= Html::a('Редактировать', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+                <?= Html::a('Удалить', ['delete', 'id' => $model->id], [
+                    'class' => 'btn btn-danger',
+                    'data' => [
+                        'confirm' => 'Are you sure you want to delete this item?',
+                        'method' => 'post',
+                    ],
+                ]) ?>
+            </div>
+            <div class="box-body">
+                <div class="product-view">
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'id',
+                            'category_id',
+                            'title',
+                            'content:ntext',
+                            'price',
+                            'old_price',
+                            'description',
+                            'keywords',
+                            //'img',
+                            [
+                                    'attribute' => 'img',
+                                    'value' => "/products/{$model->img}",
+                                    'format' => 'image',
+                            ],
+                            'is_offer',
+                        ],
+                    ]) ?>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'category_id',
-            'title',
-            'content:ntext',
-            'price',
-            'old_price',
-            'description',
-            'keywords',
-            'img',
-            'is_offer',
-        ],
-    ]) ?>
-
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
